b854b57ee251f8f53ea3f4599ff08871f61d5166
ashannon828 <ashannon828@gmail.com>
2021-02-04 22:18:55 +0200

    reformatted code and added formik

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import { Formik } from "formik"
+import { Formik } from "formik";
+import * as Yup from "yup";
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(255, "Name must be shorter than 255")
+    .required("Enter a name"),
+  email: Yup.string()
+    .email("Enter a valid email")
+    .max(255, "Email must be shorter than 255")
+    .required("Enter an email"),
+});
 
 export default function Home() {
   return (
@@ -10,25 +20,46 @@ export default function Home() {
         <title>Formik Demo</title>
       </Head>
 
-      <main >
-        <Formik initialValues={{ Name: "", email: "" }}>
+      <main>
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={{ name: "", email: "" }}
+        >
           {({ values, errors, touched, handleChange, handleBlur }) => (
             <form>
               <div>
-                <label htmlFor='name'>Name</label>
-                <input 
-                type='text' 
-                name='name' 
-                id='name' 
-                placeholder='Enter your name' 
-                onChange={handleChange} 
-                value={values.name} 
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter your name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                  className={touched.name && errors.name ? "has-error" : null}
                 />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  className={touched.email && errors.email ? "has-error" : null}
+                />
+              </div>
+              <div>
+                <button type="submit">Submit</button>
               </div>
             </form>
           )}
         </Formik>
-      </main >
-    </div >
-  )
+      </main>
+    </div>
+  );
 }
