618f593bf140dc63ad4d1c5e0baf1ed0d8ea787a
ashannon828 <ashannon828@gmail.com>
2021-02-06 13:55:45 +0200

    added the fields needed for our contact form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import { Formik } from "formik";
+import { Field, Form, Formik, FormikProps } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
@@ -23,40 +23,58 @@ export default function Home() {
       <main>
         <Formik
           validationSchema={validationSchema}
-          initialValues={{ name: "", email: "" }}
+          initialValues={{
+            name: "",
+            email: "",
+            confirmEmail: "",
+            citizenship: "",
+            reason: "",
+            relocation_country: "",
+          }}
         >
           {({ values, errors, touched, handleChange, handleBlur }) => (
-            <form>
+            <Form>
               <div>
                 <label htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Enter your name"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.name}
-                  className={touched.name && errors.name ? "has-error" : null}
-                />
+                <Field id="name" name="name" />
               </div>
               <div>
                 <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  placeholder="Enter your email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  className={touched.email && errors.email ? "has-error" : null}
-                />
+                <Field id="email" name="email" />
+              </div>
+              <div>
+                <label htmlFor="confirmEmail">Confirm Email</label>
+                <Field id="confirmEmail" name="confirmEmail" />
+              </div>
+              <div>
+                <label htmlFor="citizenship">Country of Citizenship</label>
+                <Field id="citizenship" name="citizenship" />
+              </div>
+              <div>
+                <label htmlFor="reason">How can we help?</label>
+                <Field as="select" id="reason" name="reason">
+                  <option value="residency">Residency</option>
+                  <option value="visa">Visa</option>
+                  <option value="career">Career Consultation</option>
+                </Field>
+              </div>
+              <div>
+                <label htmlFor="relocation_country">Choose a country</label>
+                <Field
+                  as="select"
+                  id="relocation_country"
+                  name="relocation_country"
+                >
+                  <option value="russia">Russia</option>
+                  <option value="turkey">Turkey</option>
+                  <option value="ukraine">Ukraine</option>
+                  <option value="other">Other</option>
+                </Field>
               </div>
               <div>
                 <button type="submit">Submit</button>
               </div>
-            </form>
+            </Form>
           )}
         </Formik>
       </main>
