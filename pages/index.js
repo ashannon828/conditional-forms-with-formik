6fe683c84385a19fddcc180b4409d17b4069b0dd
ashannon828 <ashannon828@gmail.com>
2021-02-06 16:05:05 +0200

    finished form layout and started validation

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import { Field, Form, Formik, FormikProps } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .max(255, "Name must be shorter than 255")
-    .required("Enter a name"),
-  email: Yup.string()
-    .email("Enter a valid email")
-    .max(255, "Email must be shorter than 255")
-    .required("Enter an email"),
+  name: Yup.string().required("Enter your name"),
+  email: Yup.string().email("Enter a valid email address"),
+  confirmEmail: Yup.string(),
+  citizenship: Yup.string(),
+  relocation_country: Yup.string(),
+  country_other: Yup.string(),
+  message: Yup.string(),
 });
 
 export default function Home() {
@@ -21,6 +21,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1>Residency Consultation</h1>
         <Formik
           validationSchema={validationSchema}
           initialValues={{
@@ -28,35 +29,33 @@ export default function Home() {
             email: "",
             confirmEmail: "",
             citizenship: "",
-            reason: "",
-            relocation_country: "",
+            relocation_country: "russia",
+            country_other: "",
+            message: "",
           }}
         >
           {({ values, errors, touched, handleChange, handleBlur }) => (
             <Form>
               <div>
-                <label htmlFor="name">Name</label>
-                <Field id="name" name="name" />
+                <label htmlFor="name">Name*</label>
+                <Field
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  id="name"
+                  name="name"
+                />
               </div>
               <div>
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email*</label>
                 <Field id="email" name="email" />
               </div>
               <div>
-                <label htmlFor="confirmEmail">Confirm Email</label>
+                <label htmlFor="confirmEmail">Confirm Email*</label>
                 <Field id="confirmEmail" name="confirmEmail" />
               </div>
               <div>
-                <label htmlFor="citizenship">Country of Citizenship</label>
+                <label htmlFor="citizenship">Country of Citizenship*</label>
                 <Field id="citizenship" name="citizenship" />
-              </div>
-              <div>
-                <label htmlFor="reason">How can we help?</label>
-                <Field as="select" id="reason" name="reason">
-                  <option value="residency">Residency</option>
-                  <option value="visa">Visa</option>
-                  <option value="career">Career Consultation</option>
-                </Field>
               </div>
               <div>
                 <label htmlFor="relocation_country">Choose a country</label>
@@ -71,9 +70,20 @@ export default function Home() {
                   <option value="other">Other</option>
                 </Field>
               </div>
+              {relocation_country.value === "other" && (
+                <div>
+                  <label htmlFor="country_other">Enter a country</label>
+                  <Field id="country_other" name="country_other" />
+                </div>
+              )}
               <div>
-                <button type="submit">Submit</button>
+                <label htmlFor="message">Message</label>
+                <Field as="textarea" id="message" name="message" />
               </div>
+              <div>
+                <button type="submit">Send</button>
+              </div>
+              {JSON.stringify(values)}
             </Form>
           )}
         </Formik>
